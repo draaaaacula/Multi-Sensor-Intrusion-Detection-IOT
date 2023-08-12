59d8e9545f8af7b60dca0ddb448a289431871a83
Husnain javed <hjchoudry@gmail.com>
2023-08-12 11:48:31 +0500

    notification🪡

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 import 'dracula_app.dart';
 import 'package:firebase_core/firebase_core.dart';
@@ -9,5 +10,66 @@ void main() async {
     options: DefaultFirebaseOptions.currentPlatform,
   ); //initialize firebase
 
-  runApp(const DraculaApp());
+  runApp(const PushNotificationHandler());
+}
+
+class PushNotificationHandler extends StatefulWidget {
+  const PushNotificationHandler({super.key});
+
+  @override
+  State<PushNotificationHandler> createState() =>
+      PushNotificationHandlerState();
+}
+
+class PushNotificationHandlerState extends State<PushNotificationHandler> {
+  final FirebaseMessaging _firebaseMessaging = FirebaseMessaging.instance;
+
+  void _checkPermission() async {
+    if (await _firebaseMessaging.isSupported()) {
+      _firebaseMessaging.requestPermission(
+        announcement: true, // For iOS only
+        sound: true,
+        badge: true,
+        alert: true,
+        provisional: false, // For iOS only
+      );
+    }
+  }
+
+  void _handleOnMessage() {
+    FirebaseMessaging.onMessage.listen((RemoteMessage message) {
+      try {
+        ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(
+            content: Text(message.notification!.title!),
+          ),
+        );
+      } catch (e) {
+        ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(
+            content: Text(e.toString()),
+          ),
+        );
+      }
+    });
+  }
+
+  void _handleOnMessageOpened() {
+    FirebaseMessaging.onMessageOpenedApp.listen((onData) {
+      //
+    });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _checkPermission();
+    _handleOnMessage();
+    _handleOnMessageOpened();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return const DraculaApp();
+  }
 }
